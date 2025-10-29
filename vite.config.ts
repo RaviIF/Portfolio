9589be09ea3ee69f7ef3590b7aea013fc533f417
RaviIF <ravikumar.patel.if@gmail.com>
2025-10-29 15:09:16 +0530

    Update base path for GitHub repository

--- vite.config.ts
+++ vite.config.ts
@@ -19,5 +19,5 @@ export default defineConfig(({ mode }) => ({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  base: '/' // Replace with your GitHub repo name
+  base: '/Portfolio/' // Replace with your GitHub repo name
 }));
