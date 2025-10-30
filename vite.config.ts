ef5b408beaa2dd0bf2b5caeede7c9ab717dd99ee
RaviIF <ravikumar.patel.if@gmail.com>
2025-10-30 11:20:40 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -19,5 +19,5 @@ export default defineConfig(({ mode }) => ({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  base: '/Portfolio' // Replace with your GitHub repo name
+  base: '/' // Replace with your GitHub repo name
 }));
